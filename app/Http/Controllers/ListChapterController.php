3b563f8d97251ebab05abea63167dc76517f9b83
zrt <zhangruitao@sjtu.edu.cn>
2018-11-21 11:56:30 +0800

    [UPDATE] ListChapterController

--- app/Http/Controllers/ListChapterController.php
+++ app/Http/Controllers/ListChapterController.php
@@ -35,8 +35,46 @@ class ListChapterController extends Controller
         $section_title=$_POST["sectiontitle"];
         $section_detail=$_POST["sectiondetail"];
 
-        //TODO add the materials to the data base
-
+        $sections = DB::table('sections')
+            ->join('chapter_section','sections.id','=','chapter_section.section_id')
+            ->join('chapters','chapters.id','=','chapter_section.chapter_id')
+            ->select('sections.id')
+            ->where('chapters.id','=',$chapter_id)
+            ->where('sections.rank','=',$section_rank)
+            ->get();
+        //!empty($sections['id'])
+        if(count($sections)!=0){
+            $section_id=$sections->first();
+            DB::transaction(function()use ($section_id,$chapter_id,$section_rank,$section_title,$section_detail){
+//                DB::table('chapters')
+//                    ->where('id',$chapter_id)
+//                    ->update([
+//                    //'title' => ...;
+//                ]);
+                DB::table('sections')
+                    ->where('id',$section_id)
+                    ->update([
+                    'rank' => $section_rank,
+                    'title' => $section_title,
+                    'detail' => $section_detail
+                ]);
+            });
+        }
+        else {
+            DB::transaction(function()use ($chapter_id,$section_rank,$section_title,$section_detail){
+                $section_id = DB::table('sections')
+                    ->insertGetId([
+                        'rank' => $section_rank,
+                        'title' => $section_title,
+                        'detail' => $section_detail
+                    ]);
+                DB::table('chapter_section')
+                    ->insert([
+                        'chapter_id' => $chapter_id,
+                        'section_id' => $section_id
+                    ]);
+            });
+        }
 
 
         return view('administer_material_submit');
