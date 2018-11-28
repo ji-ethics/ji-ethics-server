c54890bce8c52df4336e6806ab4d217c19e41ad9
lvkoala <lvqizhen1997@163.com>
2018-11-28 10:44:27 +0800

    [debug] web.php and material section

--- routes/web.php
+++ routes/web.php
@@ -43,12 +43,17 @@ Route::get('admin/material', function () {
     return view('administer_material_section');
 });
 
+Route::get('admin/material/add', function () {
+    return view('administer_material_add');
+});
+
 
 Route::get('chapter/{id}/section/{section_rank}', 'ChapterController@chapter');
 
 Route::post('/list_chapter', 'ListChapterController@listchapter');
 
 Route::get('surveys/{id}','SurveyController@survey');
+
 Route::post('/material_submit', 'ListChapterController@materialsubmit');
 
 
